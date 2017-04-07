1b221c9a5590c46de19cda48bc3c54e7ab8eb474
Darrell Teo <darrelltzj@gmail.com>
2017-04-08 01:49:46 +0800

    index show create

--- controllers/todos_controller.js
+++ controllers/todos_controller.js
@@ -7,51 +7,87 @@ const Todo = require('../models/todo')
 //  }
 // })
 
-function create (params) {
-  Todo.create(params, function (err, todo) {
-    if (err) {
-      console.log(err)
-      return
-    }
-    console.log(todo)
-  })
-}
+const express = require('express')
+const router = express.Router()
 
-function list () {
+router.get('/create', function (req, res) {
+  res.render('create')
+})
+
+router.post('/create', function (req, res) {
+  // console.log(req.body)
+  Todo.create(req.body, function (err, todo) {
+    if (err) console.error(err)
+    // console.log(todo)
+    todo.save()
+    res.redirect('/todos')
+  })
+})
+
+router.get('/', function (req, res) {
   Todo.find({}, function (err, todos) {
-    if (err) {
-      console.log(err)
-      return
-    }
-    console.log(todos)
+    if (err) console.error(err)
+    // console.log(todos)
+    res.render('todo', {todos: todos})
   })
-}
+})
 
-function show (id) {
-  Todo.findById(id, function (err, todo) {
-    if (err) return console.log(err)
-    console.log(todo)
+router.get('/:id', function (req, res) {
+  Todo.findById(req.params.id, function (err, todo) {
+    if (err) console.error(err)
+    // console.log(todo)
+    res.render('todo', {todos: [todo]})
   })
-}
+})
 
-function update (id, params) {
-  Todo.findOneAndUpdate({ _id: id }, params, function (err, todo) {
-    if (err) console.log(err)
-    console.log(todo)
-  })
-}
+// function create (params) {
+//   Todo.create(params, function (err, todo) {
+//     if (err) {
+//       console.log(err)
+//       return
+//     }
+//     console.log(todo)
+//   })
+// }
+//
+// function list () {
+//   Todo.find({}, function (err, todos) {
+//     if (err) {
+//       console.log(err)
+//       return err
+//     }
+//     console.log(todos)
+//     return todos
+//   })
+// }
+//
+// function show (id) {
+//   Todo.findById(id, function (err, todo) {
+//     if (err) return console.log(err)
+//     console.log(todo)
+//   })
+// }
+//
+// function update (id, params) {
+//   Todo.findOneAndUpdate({ _id: id }, params, function (err, todo) {
+//     if (err) console.log(err)
+//     console.log(todo)
+//   })
+// }
+//
+// function destroy (id) {
+//   Todo.findOneAndRemove({ _id: id }, function (err) {
+//     if (err) console.log(err)
+//     console.log('User deleted!')
+//   })
+// }
 
-function destroy (id) {
-  Todo.findOneAndRemove({ _id: id }, function (err) {
-    if (err) console.log(err)
-    console.log('User deleted!')
-  })
-}
+module.exports = router
 
-module.exports = {
-  create,
-  list,
-  show,
-  update,
-  destroy
-}
+// module.exports = {
+//   create,
+//   list,
+//   show,
+//   update,
+//   destroy
+// }
--- index.js
+++ index.js
@@ -5,3 +5,21 @@ mongoose.connect('mongodb://localhost/todo-list')
 mongoose.Promise = global.Promise
 
 // TODO. include express and body-parser, plugin in the todos controller and start listening
+var express = require('express')
+var app = express()
+var port = 3000
+
+// Layout structure settings
+var ejsLayouts = require('express-ejs-layouts')
+app.set('view engine', 'ejs')
+app.use(ejsLayouts)
+
+// Post request handler
+var bodyParser = require('body-parser')
+app.use(bodyParser.urlencoded({extended: false}))
+
+app.use('/todos', todosController)
+
+app.listen(port, function () {
+  console.log('express is running on port ' + port)
+})
